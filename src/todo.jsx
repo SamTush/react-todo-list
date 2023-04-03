ce44e2e37c8b7bd6c979f874725d2d8f83a0826b
Samson Tush <samo4421@icloud.com>
2023-04-03 21:37:20 +0300

    Add bootstrap

--- src/todo.jsx
+++ src/todo.jsx
@@ -2,11 +2,18 @@ import './todo.scss';
 
 function Todo() {
   return (
-    <div className="container main-div">
+    <div className="container-fluid main-div d-flex justify-content-center">
       <div className="row">
-        <div className="col col-12">
-          <h1>todos</h1>
-          <p>Best todo application in the world.</p>
+        <div className="col content-div">
+          <h1 className="d-flex justify-content-center">todos</h1>
+          <p className="d-flex justify-content-center">Best todo application in the world.</p>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="todo" className="form-label">
+                <input id="todo" type="password" className="form-control" />
+              </label>
+            </div>
+          </form>
         </div>
       </div>
     </div>
