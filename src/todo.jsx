58b13a4d65d3cdeb7d7cb35b90a1abaff419604f
Samson Tush <samo4421@icloud.com>
2023-04-03 19:37:51 +0300

    Add linters files and new component

---
+++ src/todo.jsx
@@ -0,0 +1,16 @@
+import './todo.scss';
+
+function Todo() {
+  return (
+    <div className="container main-div">
+      <div className="row">
+        <div className="col col-12">
+          <h1>todos</h1>
+          <p>Best todo application in the world.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
